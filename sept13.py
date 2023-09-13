a259980bdc8738d7d91941b4de2f9caab8aafdb0
Yash Saini <115717039+lonelycoder007@users.noreply.github.com>
2023-09-13 18:50:40 +0530

    Add files via upload

---
+++ sept13.py
@@ -0,0 +1,32 @@
+class Solution(object):
+    def candy(self, ratings):
+        """
+        :type ratings: List[int]
+        :rtype: int
+        """
+        n = len(ratings)
+        candies = [1] * n
+
+        # Left to right pass
+        for i in range(1, n):
+            if ratings[i] > ratings[i - 1]:
+                candies[i] = candies[i - 1] + 1
+
+        # Right to left pass
+        for i in range(n - 2, -1, -1):
+            if ratings[i] > ratings[i + 1]:
+                candies[i] = max(candies[i], candies[i + 1] + 1)
+
+        # Sum up the total candies
+        total_candies = sum(candies)
+
+        return total_candies
+
+sol=Solution()
+# Example 1
+ratings1 = [1, 0, 2]
+print(sol.candy(ratings1))  # Output: 5
+
+# Example 2
+ratings2 = [1, 2, 2]
+print(sol.candy(ratings2))  # Output: 4
