0b7599181d44916dd65fa9bfdd99f15a4dfc9660
lonelycoder007 <ys3853428@gmail.com>
2024-01-29 14:04:35 +0530

    Easy - 232 Implement Queue using Stacks

---
+++ 2024/jan-29.py
@@ -0,0 +1,21 @@
+class MyQueue:
+    def __init__(self):
+        self.s1 = []
+        self.s2 = []
+
+    def push(self, x):
+        while self.s1:
+            self.s2.append(self.s1.pop())
+        self.s1.append(x)
+        while self.s2:
+            self.s1.append(self.s2.pop())
+
+    def pop(self):
+        return self.s1.pop()
+
+    def peek(self):
+        return self.s1[-1]
+
+    def empty(self):
+        return not self.s1
+
