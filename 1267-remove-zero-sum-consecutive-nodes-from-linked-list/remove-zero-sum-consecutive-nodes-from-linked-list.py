b64d04612907b40e5125993e5836b64e09f2bf8b
Yash Kumar Saini <115717039+yashksaini-coder@users.noreply.github.com>
2024-03-12 18:03:18 +0530

    Time: 31 ms (20.51%) | Memory: 11.9 MB (92.31%) - LeetSync

---
+++ 1267-remove-zero-sum-consecutive-nodes-from-linked-list/remove-zero-sum-consecutive-nodes-from-linked-list.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def removeZeroSumSublists(self, head):
+        dummy = ListNode(0)
+        dummy.next = head
+        prefix_sum = 0
+        prefix_sums = {0: dummy}
+        current = head
+
+        while current:
+            prefix_sum += current.val
+            if prefix_sum in prefix_sums:
+                to_delete = prefix_sums[prefix_sum].next
+                temp_sum = prefix_sum + to_delete.val
+                while to_delete != current:
+                    del prefix_sums[temp_sum]
+                    to_delete = to_delete.next
+                    temp_sum += to_delete.val
+                prefix_sums[prefix_sum].next = current.next
+            else:
+                prefix_sums[prefix_sum] = current
+            current = current.next
+
+        return dummy.next
