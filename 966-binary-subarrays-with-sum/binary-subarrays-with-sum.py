e51da0e7a683e9d7824f21cbdd935cd6d5e03ba9
Yash Kumar Saini <115717039+yashksaini-coder@users.noreply.github.com>
2024-03-14 19:09:47 +0530

    Time: 203 ms (89.11%) | Memory: 14.7 MB (55.45%) - LeetSync

---
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def numSubarraysWithSum(self, nums, goal):
+        count = {0: 1}
+        curr_sum = 0
+        total_subarrays = 0
+        
+        for num in nums:
+            curr_sum += num
+            if curr_sum - goal in count:
+                total_subarrays += count[curr_sum - goal]
+            count[curr_sum] = count.get(curr_sum, 0) + 1
+
+        return total_subarrays
