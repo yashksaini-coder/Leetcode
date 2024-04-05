bbdcb5e96ebff8e34098fa924216b5161411f111
Yash Kumar Saini <115717039+yashksaini-coder@users.noreply.github.com>
2024-04-05 10:30:24 -0700

    Time: 19 ms (39.81%) | Memory: 11.5 MB (89.57%) - LeetSync

--- 1666-make-the-string-great/make-the-string-great.py
+++ 1666-make-the-string-great/make-the-string-great.py
@@ -1,13 +1,10 @@
-class Solution(object):
+class Solution:
     def makeGood(self, s):
-        """
-        :type s: str
-        :rtype: str
-        """
-        stack = []
+        stack = []  
         for char in s:
-            if stack and abs(ord(stack[-1]) - ord(char)) == 32:
+            if stack and abs(ord(char) - ord(stack[-1])) == 32:
                 stack.pop()
             else:
                 stack.append(char)
+
         return ''.join(stack)
