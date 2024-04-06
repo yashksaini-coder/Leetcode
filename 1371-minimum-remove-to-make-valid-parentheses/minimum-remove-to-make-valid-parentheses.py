d5a6d55f9f67785010b5094e699fd7415213c550
Yash Kumar Saini <115717039+yashksaini-coder@users.noreply.github.com>
2024-04-06 17:12:52 +0530

    Time: 103 ms (52.17%) | Memory: 17.2 MB (24.68%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
@@ -1,26 +1,26 @@
-class Solution(object):
+class Solution:
     def minRemoveToMakeValid(self, s):
-        """
-        :type s: str
-        :rtype: str
-        """
-        stack = []
-        indices_to_remove = set()
+        # Initialize variables
+        openParenthesesCount = 0
+        arr = list(s)
 
-        for i, char in enumerate(s):
-            if char == '(':
-                stack.append(i)
-            elif char == ')':
-                if not stack:
-                    indices_to_remove.add(i)
+        # First pass: mark excess closing parentheses with '*'
+        for i in range(len(arr)):
+            if arr[i] == '(':
+                openParenthesesCount += 1
+            elif arr[i] == ')':
+                if openParenthesesCount == 0:
+                    arr[i] = '*' # Mark excess closing parentheses
                 else:
-                    stack.pop()
+                    openParenthesesCount -= 1
 
-        indices_to_remove.update(stack)
+        # Second pass: mark excess opening parentheses from the end
+        for i in range(len(arr) - 1, -1, -1):
+            if openParenthesesCount > 0 and arr[i] == '(':
+                arr[i] = '*' # Mark excess opening parentheses
+                openParenthesesCount -= 1
+        
+        # Filter out marked characters and construct the result string
+        result = ''.join(c for c in arr if c != '*')
 
-        result = []
-        for i, char in enumerate(s):
-            if i not in indices_to_remove:
-                result.append(char)
-
-        return ''.join(result)
+        return result
