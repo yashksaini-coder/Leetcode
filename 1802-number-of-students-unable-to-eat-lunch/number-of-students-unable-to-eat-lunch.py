0c5150ef5346aed22b41a7943e2e9b932a8d76c5
Yash Kumar Saini <115717039+yashksaini-coder@users.noreply.github.com>
2024-04-08 09:08:48 +0530

    Time: 24 ms (25.18%) | Memory: 11.8 MB (16.31%) - LeetSync

---
+++ 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.py
@@ -0,0 +1,23 @@
+from collections import deque
+
+class Solution(object):
+    def countStudents(self, students, sandwiches):
+        """
+        :type students: List[int]
+        :type sandwiches: List[int]
+        :rtype: int
+        """
+        counts = [0, 0]
+        for student in students:
+            counts[student] += 1
+
+        remaining = len(sandwiches)
+        for sandwich in sandwiches:
+            if counts[sandwich] == 0:
+                break
+            if remaining == 0:
+                break
+            counts[sandwich] -= 1
+            remaining -= 1
+        
+        return remaining
